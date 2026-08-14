4a02b7e13a0cd932ded505d2a51ad799355ca29d
Arena Agent <arena-agent@arena.ai>
2026-08-14 12:49:46 +0000

    auto-sync: Fri Aug 14 12:49:46 UTC 2026

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    outDir: 'www',
+    emptyOutDir: true,
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
